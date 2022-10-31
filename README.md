429a87e42db03444222a603db3a845015fb99458
pallo rafutho <pallorafutho3@gmail.com>
2022-10-31 18:40:32 +0200

    add static page

---
+++ README.md
@@ -0,0 +1,6 @@
+this project is a static page for pallo rafutho
+
+deployed on netlify
+https://meek-daifuku-5461ce.netlify.app/
+
+branch 1
---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header">
+        
+        <p className="App-story">
+        I am Pallo Tshepang Rafutho, 21 years old, and a final year BSc Computing student at the University of South Africa.
+        I was born and bred in Kew, a Johannesburg northern suburb.
+        My interests are chess, golf, computer games, and travelling.
+        I would like to join SovTech for the following reasons:
+        The core business of SovTech is in line with my studies and aspirations. 
+        I would like to be a top business and gaming software developer.
+        Whilst still young with little responsibilities, I am available to learn and develop software, travel the world to deploy the SovTech solutions
+
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
